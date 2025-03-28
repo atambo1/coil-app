08c421196efc6b81502dadf4cd2631bc9bcda1e3
atambo1 <atambo1@gmail.com>
2025-03-28 11:20:08 -0400

    merging

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,7 +6,4 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  server: {
-    historyApiFallback: true, // <- this is dev mode fallback
-  }
 })
