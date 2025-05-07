4129ab98e06b975a7e3ac78b0283c70bbddc9f57
atambo1 <atambo1@gmail.com>
2025-05-07 15:09:45 -0400

    bigger

--- src/coils/CoilRouter.jsx
+++ src/coils/CoilRouter.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes, Route, useParams } from "react-router-dom";
-import HomePage from "../HomePage";
-import WalkPage from "../Walk";
-import WalkQuestions from "../WalkQuestions";
+import HomePage from "../pages/HomePage";
+import WalkPage from "../pages/Walk";
+import WalkQuestions from "../pages/WalkQuestions";
 
 export default function CoilRouter() {
   const { coilId } = useParams();
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage coilId={coilId} />} />
+      <Route index element={<HomePage coilId={coilId} />} />
       <Route path="walk" element={<WalkPage coilId={coilId} />} />
       <Route path="walk/questions" element={<WalkQuestions coilId={coilId} />} />
     </Routes>
